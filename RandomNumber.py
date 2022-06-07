3b243bc9587d93541941d3bb51777b1d48ebf014
anguspaterson-code <contact@anguspaterson.com>
2022-06-07 22:00:06 +1000

    learning more python

--- RandomNumber.py
+++ RandomNumber.py
@@ -1,6 +1,14 @@
 import random
 
-randy = random.sample(range(1,30), 7)
-print.randy()
+YourTicket = random.sample(range(1,30), 7)
+LotteryNum = random.sample(range(1,30), 7)
 
 
+print("Your ticket numbers: ")
+print(YourTicket)
+
+print("The lottery numbers are: ")
+print(LotteryNum)
+
+check = any(item in YourTicket for item in LotteryNum)
+print("Do you have winning numbers? : " + str(bool(check)))
